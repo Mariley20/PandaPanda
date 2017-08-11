8c7178d9525267ec860dc643fb6bdada70c8bdf6
Mariley20 <mariley2018@gmail.com>
2017-08-11 16:41:02 -0500

    css y js y index

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+for (var i = 0; i < pictures.length; i++) {
+	var tag_img = document.createElement('img');
+	
+	tag_img.setAttribute('src', pictures[i]);
+	tag_img.setAttribute('class', 'images')
+
+
+	images.appendChild(tag_img)
+}
