bc7e58fe2931e17cc7b04a052e6c18d6e9e0dab6
Mariley20 <mariley2018@gmail.com>
2017-08-11 17:34:12 -0500

    css y js y index terminado 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
 for (var i = 0; i < pictures.length; i++) {
+    var tag_div = document.createElement('div');
 	var tag_img = document.createElement('img');
+    var tag_button = document.createElement('button')
+    tag_button.setAttribute('class', 'btnX')
+    tag_button.appendChild(document.createTextNode('x'))
 	
 	tag_img.setAttribute('src', pictures[i]);
-	tag_img.setAttribute('class', 'images')
+	tag_div.setAttribute('class', 'div_images')
+    tag_div.appendChild(tag_img)
+    tag_div.appendChild(tag_button);
 
-
-	images.appendChild(tag_img)
+	images.appendChild(tag_div)
 }
